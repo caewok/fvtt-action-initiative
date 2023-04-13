e9f9095b27d1155ce45fb3398b973943ae1921d1
Michael Enion <caewok_2000@yahoo.com>
2023-04-13 16:47:38 -0700

    Working save/restore settings from config

--- scripts/settings.js
+++ scripts/settings.js
@@ -19,16 +19,90 @@ export const SETTINGS = {
   },
 
   CONFIGURE_MENU: "configure-menu",
-  SPELL_LEVELS: "spell-levels"
+  SPELL_LEVELS: "spell-levels",
+
+  DICE_FORMULAS: "dice-formulas"
 };
+
+
+export const FORMULA_DEFAULTS = {
+  MISC: {
+    BONUS: "",
+    SURPRISE: "+10",
+  },
+
+  BASIC: {
+    MELEE: "1d6",
+    MOVEMENT: "1d6",
+    RANGED: "1d4",
+    OTHER: "1d6",
+    SPELL: "1d10",
+    SWAP: "1d6"
+  },
+
+  // Added dynamically
+  WEAPON_TYPES: {},
+  WEAPON_PROPERTIES: {},
+}
+
+/**
+ * Take an object with key(s) separated by commas in the name, and
+ * assign the value to each key in that name.
+ * See https://stackoverflow.com/questions/14743536/multiple-key-names-same-pair-value
+ * @param {object} obj
+ * @returns {object}
+ * @example
+ * expand({ "thanksgiving day, thanksgiving, t-day": 1});
+ */
+function expand(obj) {
+    var keys = Object.keys(obj);
+    for (var i = 0; i < keys.length; ++i) {
+        var key = keys[i],
+            subkeys = key.split(/,\s?/),
+            target = obj[key];
+        delete obj[key];
+        subkeys.forEach(function(key) { obj[key] = target; })
+    }
+    return obj;
+}
+
+function dnd5eDefaultWeaponProperties() {
+  const keys = Object.keys(CONFIG.DND5E.weaponProperties).join(",");
+  const props = expand({[`${keys}`]: "0"});
+
+  // Set some of the defaults
+  props.fin = "-1";
+  props.hvy = "+2";
+  props.lod = "1d4";
+  props.thr = "-1";
+  props.two = "1d4";
+
+  return props;
+}
+
+function dnd5eDefaultWeaponTypes() {
+  const keys = Object.keys(CONFIG.DND5E.weaponTypes).join(",");
+  const props = expand({[`${keys}`]: "0"});
+
+  // Set some of the defaults
+  props.natural = "1";
+  props.siege = "2d10";
+  props.simpleM = "1d6";
+  props.martialM = "1d8";
+  props.simpleR = "1d4";
+  props.martialR = "1d6";
+
+  return props;
+}
+
+function defaultDiceFormulaObject() {
+  const flat = flattenObject(FORMULA_DEFAULTS);
+  Object.keys(flat).forEach(key => flat[key] = "");
+  return flat;
+}
 
 export function getSetting(settingName) {
   return game.settings.get(MODULE_ID, settingName);
-}
-
-export async function toggleSetting(settingName) {
-  const curr = getSetting(settingName);
-  return await game.settings.set(MODULE_ID, settingName, !curr);
 }
 
 export async function setSetting(settingName, value) {
@@ -36,7 +110,6 @@ export async function setSetting(settingName, value) {
 }
 
 export function registerSettings() {
-
   game.settings.registerMenu(MODULE_ID, SETTINGS.CONFIGURE_MENU, {
     name: game.i18n.localize(`${MODULE_ID}.settings.${SETTINGS.CONFIGURE_MENU}.Name`),
     hint: game.i18n.localize(`${MODULE_ID}.settings.${SETTINGS.CONFIGURE_MENU}.Hint`),
@@ -69,6 +142,18 @@ export function registerSettings() {
     scope: "world",
     config: true
   });
+
+
+  // Register defaults for weapon types and properties
+  FORMULA_DEFAULTS.WEAPON_TYPES = dnd5eDefaultWeaponTypes();
+  FORMULA_DEFAULTS.WEAPON_PROPERTIES = dnd5eDefaultWeaponProperties();
+  game.settings.register(MODULE_ID, SETTINGS.DICE_FORMULAS, {
+    name: `${MODULE_ID}.settings.${SETTINGS.DICE_FORMULAS}.Name`,
+    type: Object,
+    default: defaultDiceFormulaObject(),
+    scope: "world",
+    config: false
+  });
 }
 
 class ActionConfigureMenu extends FormApplication {
@@ -92,20 +177,28 @@ class ActionConfigureMenu extends FormApplication {
 
   getData() {
     const data = super.getData();
-    data.actioninitiative = {};
+    const formulae = getSetting(SETTINGS.DICE_FORMULAS);
+    data.dice = expandObject(formulae);
+    data.defaultDice = FORMULA_DEFAULTS;
     return data;
   }
 
   async _updateObject(_, formData) {
-    const data = expandObject(formData);
-    for ( const [key, formula] of Object.entries(data.dice) ) {
-      if ( formula === "" ) continue;
+    const toUpdate = Object.entries(formData).filter(([key, formula]) => {
+      if ( formula === "" ) return false;
       if ( !Roll.validate(formula) ) {
         ui.notifications.warn("Die formula for ${key} is not valid.");
-        continue;
+        return false;
       }
-      setSetting(`${key}Dice`, formula);
-    }
+      return true;
+    });
 
+    if ( !toUpdate.length ) return;
+
+    const diceFormulas = getSetting(SETTINGS.DICE_FORMULAS)
+    for ( const [key, formula] of toUpdate ) {
+      diceFormulas[key] = formula;
+    }
+    setSetting(SETTINGS.DICE_FORMULAS, diceFormulas);
   }
 }
